896eba01068e9e08429b4c7989d5c39e4f9b02b7
biswanath <biswanaths@gmail.com>
2022-12-31 12:47:32 +0530

    use socket as continous channel

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
-use std::net::UdpSocket;
+use std::{net::UdpSocket, str};
+
+fn print_type_of<T>(_: T) {
+    println!("{}", std::any::type_name::<T>())
+}
 
 fn main() -> std::io::Result<()> {
     {
         let socket = UdpSocket::bind("127.0.0.1:34001")?;
 
-        let mut buffer = [0; 10];
-        let (amt, src) = socket.recv_from(&mut buffer)?;
+        loop {
+            let mut buffer = [0; 10];
+            let (amt, src) = socket.recv_from(&mut buffer)?;
+            let request = str::from_utf8(&buffer).unwrap().to_string();
 
-        let buffer = &mut buffer[..amt];
-        buffer.reverse();
-        socket.send_to(buffer, &src)?;
+            match request.as_str() {
+                "exit" => return Ok(()),
+                _ => {
+                    println!("requested {}", request);
+                }
+            }
+
+            let buffer = &mut buffer[..amt];
+
+            buffer.reverse();
+            socket.send_to(buffer, &src)?;
+        }
     }
-
-    Ok(())
 }
